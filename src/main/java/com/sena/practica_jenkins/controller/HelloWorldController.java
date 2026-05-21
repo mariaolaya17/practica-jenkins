430d45a2513316e2cee277a060d23c47d81c1c33
mariaolaya17 <mariadelosangelesolayagar@gmail.com>
2026-05-21 13:52:29 -0500

    Webhook funcionando

--- src/main/java/com/sena/practica_jenkins/controller/HelloWorldController.java
+++ src/main/java/com/sena/practica_jenkins/controller/HelloWorldController.java
@@ -12,6 +12,6 @@ public class HelloWorldController {
     @GetMapping("")
     public String getMethodName() {
    
-        return "Hola mundo";
+        return "Hola Jenkins test final";
     }
 }
